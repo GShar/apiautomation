d10d9f0e33466f05babfecf2ace0d3d7a713b381
GShar <gsharma.sharma@gmail.com>
2018-08-15 19:37:45 +0530

    [gopal] | add test class

---
+++ src/test/java/ApiExecutorTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+public class ApiExecutorTest {
+
+    @Test
+    public void verifyName(){
+        System.out.println("test");
+    }
+
+    @Test
+    public void verifyCanRelistisTrue(){
+        System.out.println("test");
+    }
+
+    @Test
+    public void verifyDescriptionTextinPromotions(){
+
+    }
+
+
+
+
+
+}
