f5bd2abf3d580ef5aed4595245a646d83e4e1722
GShar <gsharma.sharma@gmail.com>
2018-08-18 09:38:44 +0530

    [gopal] | create setup method for the test class and set api configuration file values

--- src/test/java/ApiExecutorTest.java
+++ src/test/java/ApiExecutorTest.java
@@ -1,6 +1,18 @@
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class ApiExecutorTest {
+
+    public static final String TESTAPI_PROPERTIES = "testapi.properties";
+
+    @Before
+    public void setUp() throws IOException {
+        ConfigurationFileReader readconfig= new ConfigurationFileReader(TESTAPI_PROPERTIES);
+        ApiRequest request= new ApiRequest(readconfig.getRequestUrl(), readconfig.getRequestMethod());
+        request.createAndSendRequest();
+    }
 
     @Test
     public void verifyName(){
